d8db1a85a4de6eec6de6aed96403e76c17f35871
heatherthomas513 <heatherthomas513@gmail.com>
2025-06-29 15:36:10 -0400

    Added basic sign up form and submit button, styled it with flex box and added a js function to make it hidden until the Sign Up button is clicked.

--- javascript.js
+++ javascript.js
@@ -2,3 +2,10 @@ function playSound() {
     var sound = new Audio('quack.mp3');
     sound.play();
 }
+
+document.getElementById("signUpBtn").addEventListener("click", () => signUpForm());
+
+function signUpForm() {
+    document.getElementById("signUpBtn").classList.add("hidden");
+    document.getElementById("signUpFormSec").classList.remove("hidden");
+}
